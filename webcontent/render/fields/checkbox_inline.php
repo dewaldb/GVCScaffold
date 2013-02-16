7ffd10c299db6c4afc729b39743e47e1360508a2
Executor <admin@quasistellar.co.za>
2013-02-16 11:17:53 +0200

    added is_array() check on $value

--- webcontent/render/fields/checkbox_inline.php
+++ webcontent/render/fields/checkbox_inline.php
@@ -6,11 +6,15 @@ if($readonly===true) {
 }
 $input = "";
 foreach($items as $itemLabel=>$itemValue) {
+    $checked="";
+    if((is_array($values) && array_search($itemValue[0],$values)!==false) || $values==$itemValue[0]) {
+        $checked="checked='yes'";
+    }
     if($itemValue[1]!=false) {
-        $input.= "<label class='checkbox'><input ". (array_search($itemValue[0],$values)!==false ? "checked='yes'" : ""). " type='checkbox' id='{$form_name}_{$id}' name='{$form_name}_{$id}[]' value='{$itemValue[0]}' {$readonly} class='$class' />{$itemLabel}</label>";
+        $input.= "<label class='checkbox'><input $checked type='checkbox' id='{$form_name}_{$id}' name='{$form_name}_{$id}[]' value='{$itemValue[0]}' {$readonly} class='$class' />{$itemLabel}</label>";
     } else {
         $input.= "<label class='checkbox'>";
-        $input.= "<input ". (array_search($itemValue[0],$values)!==false ? "checked='yes'" : ""). " type='checkbox' id='{$form_name}_{$id}' name='{$form_name}_{$id}[]' value='{$itemValue[0]}' {$readonly} disabled='disabled' class='$class' />";
+        $input.= "<input $checked type='checkbox' id='{$form_name}_{$id}' name='{$form_name}_{$id}[]' value='{$itemValue[0]}' {$readonly} disabled='disabled' class='$class' />";
         $input.= "<input type='hidden' id='{$form_name}_{$id}' name='{$form_name}_{$id}[]' value='". (array_search($itemValue[0],$values)!==false ? "{$itemValue[0]}" : ""). "' />";
         $input.= "{$itemLabel}";
         $input.= "</label>";

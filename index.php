52864c5a9e44141a0019dc3760c13abbc1561b68
Dewald <Dewald@Dewald-PC>
2013-12-03 17:32:37 +0200

    added Mailer.php and Message.php, updated DataSource.php folder, added Get value, dev, and SiteName and SiteEmail Globals

--- index.php
+++ index.php
@@ -2,13 +2,21 @@
 ini_set('error_reporting', E_ALL);
 ini_set('display_errors', 1);
 
+include_once("Mailer.php");
+include_once("Message.php");
 include_once("SessionUser.php");
 include_once("Permissions.php");
 include_once("Forms.php");
-include_once("DataSource.php");
+include_once("datasource/DataSource.php");
 include_once("Controller.php");
 include_once("Render.php");
 include_once("Router.php");
+
+// place website in development mode
+$_GET["dev"] = true;
+
+$GLOBALS["SiteName"] = "Fund Raiser Backend";
+$GLOBALS["SiteEmail"] = "admin@fundraiser.com";
 
 Controller::includeAll();
 Forms::setUploadPath("uploads");

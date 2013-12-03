1552ecc2bc6edbd4d317285ea160ab34cf3b4e8e
dewaldb <admin@quasistellar.co.za>
2013-12-03 16:54:24 +0200

    more informative footer message

--- webcontent/layout/footer.php
+++ webcontent/layout/footer.php
@@ -1,5 +1,9 @@
 <div id="footer">
     <div id="footer-content">
-        <p class="muted credit"><?php print "| ".(count(SessionUser::getUserRoles()) ? implode(", ", SessionUser::getUserRoles()) : "anonymous")." |";?></p>
+        <?php if(SessionUser::isValidUser()) { ?>
+            <p class="muted credit">You are logged in with the following role(s): <?php print (count(SessionUser::getUserRoles()) ? implode(", ", SessionUser::getUserRoles()) : "anonymous");?>.</p>
+        <?php } else { ?>
+            <p class="muted credit">You are not logged in.</p>
+        <?php } ?>
     </div>
 </div>

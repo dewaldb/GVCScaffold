b0479254f1ca44d6c57391c6dea7065b196c01ff
Dewald <Dewald@Dewald-PC>
2013-12-03 16:56:06 +0200

    fixed incorrect Bootstrap class name (form-actions)

--- webcontent/_generic_edit.php
+++ webcontent/_generic_edit.php
@@ -2,7 +2,7 @@
     <?php print Render::pageTitle($GLOBALS["title"]); // use the render class to add proper css to the page title which is loaded from a template ?>
     <form name='<?php print $form_name; ?>' class='form-horizontal' method='POST' enctype='multipart/form-data'>
         <?php print Render::formFields($form_name,$fields,Forms::getState($form_name)); ?>
-        <div class='controls'>
+        <div class='form-actions'>
             <input type='submit' name='<?php print $form_name; ?>_submit' value='Save' class='btn btn-primary' />
             <input type='submit' name='<?php print $form_name; ?>_submit' value='Cancel' class='btn btn-inverse' />
         </div>
